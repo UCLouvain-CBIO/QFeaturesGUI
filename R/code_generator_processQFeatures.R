7dd0cace975b5a826795ef913e50b4fc9a11de0d
loguille <loic.guille@uclouvain.be>
2026-04-07 10:05:25 +0200

    Add documentation for each new function

--- R/code_generator_processQFeatures.R
+++ R/code_generator_processQFeatures.R
@@ -1,31 +1,56 @@
+#' @title Code generator for aggregation tab
+#' @param method the method used to do the aggregation
+#' @param fcol `character(1)` naming a `rowData` variable that defines how to aggregate
+#'   the features within each assay. This variable is either a character or a (possibly
+#'   sparse) matrix.
+#'
+#' @return code line generated
+#' @rdname codeGeneratorAggregation
+#' @keywords internal
+#'
+
 codeGeneratorAggregation <- function(method, fcol){
   codeLines <- sprintf(
     "qf <- lapply(seq_along(qf), function(i) {
-    \tname <- names(qf)[i]
-    \taggregateFeatures(
-    \t\tobject = qf[[name]],
-    \t\tmethod = %s,
-    \t\trunCol = '%s',
-    \t\tna.rm = TRUE
-    \t)
-    })",
+  \tname <- names(qf)[i]
+  \taggregateFeatures(
+  \t\tobject = qf[[name]],
+  \t\tmethod = %s,
+  \t\trunCol = '%s',
+  \t\tna.rm = TRUE
+  \t)
+  })",
     method,
     fcol
   )
   codeLines
 }
 
+#' @title Code generator for join tab
+#'
+#' @return code line generated
+#' @rdname codeGeneratorJoin
+#' @keywords internal
+#'
 
 codeGeneratorJoin <- function(){
   codeLines <- sprintf(
     "qf <- joinAssays(
-    \tx = qf,
-    \ti = names(qf)
-    )"
+  \tx = qf,
+  \ti = names(qf)
+  )"
   )
   codeLines
 }
 
+#' @title Code generator for filtering missing values tab
+#' @param pNA `float` threshold for filtering missing value
+#' @param type feature or sample
+#'
+#' @return code lines generated
+#' @rdname codeGeneratorNA
+#' @keywords internal
+#'
 
 codeGeneratorNA <- function(pNA, type){
   if(type == "features"){
@@ -49,6 +74,13 @@ codeGeneratorNA <- function(pNA, type){
   codeLines
 }
 
+#' @title Code generator for normalisation tab
+#' @param method the method used to do the normalisation
+#' 
+#' @return code lines generated
+#' @rdname codeGeneratorNormalisation
+#' @keywords internal
+#'
 
 codeGeneratorNormalisation <- function(method){
   codeLines <- sprintf(
@@ -62,46 +94,58 @@ codeGeneratorNormalisation <- function(method){
   codeLines
 }
 
+#' @title Code generator for filtering tab
+#' @param condition A list of filtering condition specifications
+#' @param type feature or sample
+#'
+#' @return code lines generated
+#' @rdname codeGeneratorFiltering
+#' @keywords internal
+#'
 
 codeGeneratorFiltering <- function(condition, type){
-  if(type == "features"){
-    final = ""
-    for(i in 1:length(condition)){
-      build_condition <- paste0("filterFeatures(~",condition[[i]]$annotation, " ", condition[[i]]$operator," ")
-      if(is.numeric(condition[[i]]$value[[1]])){
-        vector <- paste0("c(", paste(condition[[i]]$value, collapse = ","), "))")
-      } else{
-        vector <- paste0("c('", paste(condition[[i]]$value, collapse = "','"), "'))")
-      }
-      build_condition <- paste0(build_condition, vector)
-      final <- paste0(final, " |> ", build_condition)
-    }
-    condition_used <- paste0("qf", final)
-    codeLines <- sprintf(
-      "qf <- %s",
-      condition_used
-    )
+  if(length(condition) == 0){
+    codeLines <- sprintf("#No %s filtering applied", type)
   } else {
-    final = ""
-    for(i in 1:length(condition)){
-      build_condition <- paste0("colData(qf)$", condition[[i]]$annotation, " ", condition[[i]]$operator, " ")
-      if(is.numeric(condition[[i]]$value[[1]])){
-        vector <- paste0("c(", paste(condition[[i]]$value, collapse = ","), ")")
-      } else{
-        vector <- paste0("c('", paste(condition[[i]]$value, collapse = "','"), "')")
+    if(type == "features"){
+      final = ""
+      for(i in 1:length(condition)){
+        build_condition <- paste0("filterFeatures(~",condition[[i]]$annotation, " ", condition[[i]]$operator," ")
+        if(is.numeric(condition[[i]]$value[[1]])){
+          vector <- paste0("c(", paste(condition[[i]]$value, collapse = ","), "))")
+        } else{
+          vector <- paste0("c('", paste(condition[[i]]$value, collapse = "','"), "'))")
+        }
+        build_condition <- paste0(build_condition, vector)
+        final <- paste0(final, " |> ", build_condition)
       }
-      build_condition <- paste0(build_condition, vector)
-      if(i == 1){
-        final <- build_condition
-      } else {
-        final <- paste0(final, " & ", build_condition)
+      condition_used <- paste0("qf", final)
+      codeLines <- sprintf(
+        "qf <- %s",
+        condition_used
+      )
+    } else {
+      final = ""
+      for(i in 1:length(condition)){
+        build_condition <- paste0("colData(qf)$", condition[[i]]$annotation, " ", condition[[i]]$operator, " ")
+        if(is.numeric(condition[[i]]$value[[1]])){
+          vector <- paste0("c(", paste(condition[[i]]$value, collapse = ","), ")")
+        } else{
+          vector <- paste0("c('", paste(condition[[i]]$value, collapse = "','"), "')")
+        }
+        build_condition <- paste0(build_condition, vector)
+        if(i == 1){
+          final <- build_condition
+        } else {
+          final <- paste0(final, " & ", build_condition)
+        }
       }
+      condition_used <- paste0("qf[, ", final, "]")
+      codeLines <- sprintf(
+        "qf <- %s",
+        condition_used
+      )
     }
-    condition_used <- paste0("qf[, ", final, "]")
-    codeLines <- sprintf(
-      "qf <- %s",
-      condition_used
-    )
   }
   codeLines
 }
