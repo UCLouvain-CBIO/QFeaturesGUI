cf741ef2df782f11c610e80e5c88ae7e307ebf69
loguille <loic.guille@uclouvain.be>
2026-04-27 14:46:09 +0200

    change initial sentence for loading dataset

--- R/server_module_summary_tab.R
+++ R/server_module_summary_tab.R
@@ -111,7 +111,7 @@ server_module_summary_tab <- function(id) {
                     paste0("# Generated on: ", Sys.time()),
                     "",
                     "####################################\n######### Package loading ##########\n####################################\nlibrary(QFeatures)\n",
-                    "####################################\n########## Load dataset ############\n####################################\n#Replace 'myDataset' with the path towards your RDS or QFeatures file\nqf <- readRDS('myDataset') \n"
+                    "####################################\n########## Load dataset ############\n####################################\n##Replace 'myDataset' with the path towards your initial Qfeatures .rds file.\n##Or directly assign your initial QFeatures object to qf.\n"
                   ),
                   r_file
                 )
